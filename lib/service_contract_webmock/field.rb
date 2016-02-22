c1cd6fe0ce2f907cab7ee3a4f9c817b203873ba4
Donald Plummer <dplummer@avvo.com>
2016-02-21 23:50:43 -0800

    enum type support

--- lib/service_contract_webmock/field.rb
+++ lib/service_contract_webmock/field.rb
@@ -43,6 +43,8 @@ module ServiceContractWebmock
         case type.items.type_sym
         when :int
           "[,\\d]+"
+        when :enum
+          "(#{type.items.symbols.map{|symbol| "#{symbol}(\..+)?"}.join("|")})"
         else
           raise "unhandled array subtype #{type.items.type_sym}"
         end
